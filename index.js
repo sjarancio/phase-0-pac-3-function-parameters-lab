a841653f77fbf1756d976c3a59f32a9181427614
Stephen Arancio <sjarancio@gmail.com>
2021-05-27 15:17:10 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);
+}
+
+  introduction('Josh');
+
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+introduction('Steve', 'Javascript')
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+introduction('Steve')
